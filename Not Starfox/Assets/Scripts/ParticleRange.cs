8c020480ff40bf5b685d79ee89aef0348ff23110
Jonathan Ramanujam <75343684+jonathanramanujam@users.noreply.github.com>
2022-12-07 20:16:07 -0700

    Buildings and trees

---
+++ Not Starfox/Assets/Scripts/ParticleRange.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleRange : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float enabledRange;
+    private ParticleSystem[] particles;
+    private bool isPlaying = false;
+    private float distanceToPlayer;
+    public float multiplier = 1;
+
+    private void Start() {
+        particles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToPlayer = transform.position.magnitude - player.transform.position.magnitude;
+
+        Debug.Log($"Distance: {distanceToPlayer}");
+        if (distanceToPlayer <= enabledRange)
+        {
+            if (isPlaying) { return; }
+
+            PlayParticles();
+            isPlaying = true;
+        }
+        else
+        {
+            if (!isPlaying) { return; }
+
+            foreach (ParticleSystem particle in particles)
+            {
+                particle.Stop();
+            }
+            isPlaying = false;
+        }
+    }
+
+    private void PlayParticles()
+    {
+        foreach (ParticleSystem particle in particles)
+            {
+				ParticleSystem.MainModule mainModule = particle.main;
+				mainModule.startSizeMultiplier *= multiplier;
+                mainModule.startSpeedMultiplier *= multiplier;
+                mainModule.startLifetimeMultiplier *= Mathf.Lerp(multiplier, 1, 0.5f);
+                particle.Clear();
+                particle.Play();
+            }
+    }
+}
